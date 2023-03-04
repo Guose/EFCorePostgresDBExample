c83f514432180dccff0a5d797b2e6173d67231ac
Justin Elder <guose79@gmail.com>
2023-03-03 22:55:17 -0800

    Setup modelBuilder table relationships

--- NinjaDomain.Data/Data/NinjaDbContext.cs
+++ NinjaDomain.Data/Data/NinjaDbContext.cs
@@ -7,8 +7,28 @@ namespace NinjaDomain.Data.Data
     {
         public NinjaDbContext(DbContextOptions<NinjaDbContext> options) : base(options) { }
 
-        public DbSet<Ninja>? NinjasTbl { get; set; }
-        public DbSet<Clan>? ClansTbl { get; set; }
-        public DbSet<NinjaEquipment>? NinjaEquipmentTbl { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Ninja>(entity => 
+            {
+               entity.HasOne(c => c.Clan) 
+                    .WithMany(n => n.Ninjas!)
+                    .HasForeignKey(nc => nc.ClanId)
+                    .OnDelete(DeleteBehavior.Restrict)
+                    .HasConstraintName("FK_Ninja_Clan");
+
+                entity.HasMany(ne => ne.EquipmentOwned!)
+                    .WithOne(n => n.Ninja)
+                    .HasForeignKey(en => en.NinjaId)
+                    .OnDelete(DeleteBehavior.Restrict)
+                    .HasConstraintName("FK_EquipmentOwned_Ninja");
+            });
+        }
+
+        public virtual DbSet<Ninja>? NinjasTbl { get; set; }
+        public virtual DbSet<Clan>? ClansTbl { get; set; }
+        public virtual DbSet<NinjaEquipment>? NinjaEquipmentTbl { get; set; }
     }
 }
